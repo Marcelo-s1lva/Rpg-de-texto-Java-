6beb49e470b153e1952c4d01b10c47c534b13745
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 12:40:00 -0300

    Update Shop.java

--- RpgTexto/src/gameSystems/locationsSystem/shopSystem/Shop.java
+++ RpgTexto/src/gameSystems/locationsSystem/shopSystem/Shop.java
@@ -9,11 +9,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public abstract class Shop extends Location {
+    //estoque da loja
     private List<Item> stock = new ArrayList<>();
     private List<Integer> costs = new ArrayList<>();
     private Player client;
 
-
+    //comprar um item
     private void buy(Item itemToBuy, int cost, Player player){
         if (player.getMoney() >= cost){
             player.setMoney(player.getMoney() - cost);
@@ -25,20 +26,23 @@ public abstract class Shop extends Location {
         }
     }
 
-
+    //retorna o cliente
     public Player getClient() {
         return client;
     }
 
+    //adciona um cliente
     public void setClient(Player client){
         this.client = client;
     }
 
+    //adciona um item no slot
     protected void addItem(Item item, int cost){
         stock.add(item);
         costs.add(cost);
     }
 
+    //comprar um item com base em um valor inteiro
     private void buyItem(int selectedItem){
         if (selectedItem < stock.size() && client != null){
             buy(stock.get(selectedItem), costs.get(selectedItem),client);
@@ -49,6 +53,7 @@ public abstract class Shop extends Location {
         }
     }
 
+    //carrega o menu de compra
     protected void shopBlock(){
         if (!stock.isEmpty()) {
             boolean Selected = false;
