036b66f0bab84604507076b9e8b705da18721c52
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 12:38:18 -0300

    Update Location.java

--- RpgTexto/src/gameSystems/locationsSystem/Location.java
+++ RpgTexto/src/gameSystems/locationsSystem/Location.java
@@ -1,14 +1,14 @@
 package gameSystems.locationsSystem;
 
-
+//representa uma localização
 public abstract class Location {
     private boolean inLocation = false;
 
-
+    //carrega localização
     public void loadLocation(){
         structure();
     }
-
+    //estrutura while do código
     private void structure(){
         inLocation = true;
         while(inLocation == true){
@@ -17,6 +17,7 @@ public abstract class Location {
         onExit();
     }
 
+    //bloco de código da estrutura
     protected void locationBlock(){
         System.out.println("In location");
         int test = 0;
@@ -25,10 +26,12 @@ public abstract class Location {
         }
     }
 
+    //bloco de código chamado ao sair da estrutura
     protected void onExit(){
         System.out.println("Set Exit output");
     }
 
+    //sair
     protected void exit(){
         inLocation = false;
     }
