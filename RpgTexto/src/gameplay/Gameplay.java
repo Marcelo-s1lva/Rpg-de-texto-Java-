5c42791026c23879a51376ffd28755ab1b7d9ea7
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 10:57:28 -0300

    Update Gameplay.java

--- RpgTexto/src/gameplay/Gameplay.java
+++ RpgTexto/src/gameplay/Gameplay.java
@@ -5,6 +5,7 @@ import gameSystems.dialogueSystem.Dialogues;
 
 
 public class Gameplay {
+    //Adciona instâncias dos times, dialogos. etc
     public static Dialogues dialogues = new Dialogues();
     public static Player mainPlayer = new Player();
     public static Displays display = new Displays();
