e51ac731b2313348c6a14c0ac017d8765bd005ba
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 12:42:17 -0300

    Update Team.java

--- RpgTexto/src/gameSystems/teamSystem/Team.java
+++ RpgTexto/src/gameSystems/teamSystem/Team.java
@@ -3,6 +3,7 @@ package gameSystems.teamSystem;
 import gameSystems.characterSystem.Character;
 import gameSystems.characterSystem.EmptyChar;
 
+//classe que representa um time
 public class Team {
     private Character[] team;
     private boolean isBot;
@@ -11,6 +12,7 @@ public class Team {
         return isBot;
     }
 
+    //cria um novo time
     public Team(int size, boolean isBot){
         team = new Character[size];
         for (int i = 0;i < size ; i++ ){
@@ -20,6 +22,7 @@ public class Team {
         this.isBot = isBot;
     }
 
+    //outra forma de criar um time(aqui, a variável isBot será por padrão true)
     public Team(int size){
         team = new Character[size];
         for (int i = 0;i < size ; i++ ){
@@ -37,7 +40,7 @@ public class Team {
         team[slot] = character;
     }
 
-    //these parameters are character positions in the team
+    //troca as posições no time
     public void swapCharacterPosition(int character1, int character2){
         if (isOnLength(character1) && isOnLength(character2)){
             Character swap = team[character1].copyObject();
@@ -46,6 +49,7 @@ public class Team {
         }
     }
 
+    //verifica se um valor é válido
     private boolean isOnLength(int value){
         if (value >= 0 && value < team.length){
             return true;
@@ -55,6 +59,7 @@ public class Team {
         }
     }
 
+    //troca as posições de personagens em diferentes inventários
     public static void swapCharacterPosition(Character[] character1 ,int slot1 , Character[] character2, int slot2){
         if (slot1 >= 0 && slot1 < character1.length && slot2 >= 0 && slot2 < character2.length){
             Character swap = character1[slot1].copyObject();
