2df9829d16514028a1e78f81336af155f4c6c20f
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 12:27:02 -0300

    Update Example.java

--- RpgTexto/src/gameSystems/dialogueSystem/dialogues/Example.java
+++ RpgTexto/src/gameSystems/dialogueSystem/dialogues/Example.java
@@ -2,10 +2,13 @@ package gameSystems.dialogueSystem.dialogues;
 
 import gameSystems.dialogueSystem.Dialogue;
 
+//Exemplo de dialogo
 public class Example extends Dialogue {
     public Example(){
+        //adiciona caixas de texto
         textBlocks.add("this is an example of a dialogue in my RPG game");
         textBlocks.add("in this dialogue system, you can easily create and run a dialogue");
+        //Mensagem final
         endMessage = "Enjoy it :)";
     }
 }
