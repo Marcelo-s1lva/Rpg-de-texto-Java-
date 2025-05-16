eb38e0c148371811297a65ddce75730d4cabca00
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 11:15:02 -0300

    Update Battles.java

--- RpgTexto/src/gameSystems/battleSystems/Battles.java
+++ RpgTexto/src/gameSystems/battleSystems/Battles.java
@@ -3,11 +3,14 @@ package gameSystems.battleSystems;
 import gameSystems.teamSystem.Team;
 import gameSystems.teamSystem.Teams;
 
+//Adciona instâncias de batalhas
 public class Battles {
+    private Teams teams = new Teams(); //Adciona os times
     public Battle main_01;
-    private Teams teams = new Teams();
+
 
     public Battles(){
+        //Exembplo de uma batalaha sendo criada
         main_01 = new Battle();
         main_01.setEnemy(teams.main_01);
     }
