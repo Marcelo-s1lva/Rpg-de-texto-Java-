51e065c37fe98ca354e4bc7d434a4ab530a0f909
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 11:52:42 -0300

    Update Default.java

--- RpgTexto/src/gameSystems/characterSystem/characters/Default.java
+++ RpgTexto/src/gameSystems/characterSystem/characters/Default.java
@@ -2,14 +2,16 @@ package gameSystems.characterSystem.characters;
 
 import gameSystems.characterSystem.Character;
 
+//Exemplo de personagem padrão
 public class Default extends Character {
+    //Construção de personagem
     public Default(){
-        setName("None");
-        setDefaultStrength(0);
-        setDefaultMaxHealth(0);
-        setUltiDelay(1);
-        setUltiName("None");
-        setUltiStrength(0);
-        setClassId(2);
+        setName("None");//Nome do personagem
+        setDefaultStrength(0);// Força padrão
+        setDefaultMaxHealth(0); //vida maxima padrão
+        setUltiDelay(1); //delay para ulti
+        setUltiName("None"); //Nome da ulti
+        setUltiStrength(0); //Força da ulti(em acréscimo de porcentagem)
+        setClassId(2); //Id da classe
     }
 }
