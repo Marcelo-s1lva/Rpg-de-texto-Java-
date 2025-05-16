9c82e6bf269d5ce9d8db43b9e8312eddcc2c22f0
Marcelo Silva <marceloadsilva323@gmail.com>
2025-05-16 10:46:58 -0300

    Update Main.java

--- RpgTexto/src/Main.java
+++ RpgTexto/src/Main.java
@@ -1,33 +1,40 @@
 import gameplay.*;
 import java.util.Scanner;
 
-public class Main {
 
+//Exemplo de Gameplay
+public class Main {
+    
     public static void main(String[] args) {
+        //Adiciona persongagem principal
         Player player = Gameplay.mainPlayer;
         Scanner insert = new Scanner(System.in);
 
-
+        //Coloca os dialogos iniciais
         Gameplay.dialogues.dialogoInicial.runDialogue();
         System.out.println("");
+        //Solicitção de inserção de nome
         System.out.println("Insira seu nome:");
         player.setProtagonist(insert.nextLine());
-        player.setMoney(0);
+        player.setMoney(0);  //Coloca uma quantia inicial para o jogador
+        
         System.out.println("Bem vindo: "+ player.getProtagonistName());
         System.out.println("");
         System.out.println("Aperte qualquer tecla para iniciar");
         insert.nextLine();
         System.out.println("");
-        System.out.println(Gameplay.mainPlayer.getProtagonistName());
         System.out.println("");
         System.out.println("");
         System.out.println("");
         System.out.println("------Introdução------");
-        Gameplay.dialogues.introduction.runDialogue();
+        Gameplay.dialogues.introduction.runDialogue();//Carregar Dialogo de introdução
 
         Gameplay.battles.main_01.start(player.playerTeam);
-        Util.BattleLoop(Gameplay.battles.main_01);
-        System.out.println("You Win");
+        Util.BattleLoop(Gameplay.battles.main_01); //Verifica se o jagador perdeu a batalha, e colocá a batalha em loop até que o jogador vença
+        System.out.println("You Win");  //Caso o jogador vença a batalha
+
+
+        //Em breve: Jogo completo, com mais personagens, localizações, missões. etc
     }
 }
 
